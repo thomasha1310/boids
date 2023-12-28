7bd705ad059b47a43a6844103dd1d110c8675a0b
Thomas Ha <96492327+tha2026@users.noreply.github.com>
2023-12-28 11:27:37 -0500

    Set up FLockAgent script

--- Assets/Scripts/FlockAgent.cs
+++ Assets/Scripts/FlockAgent.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider2D))]
 public class FlockAgent : MonoBehaviour
 {
+
+    Collider2D agentCollider;
+    public Collider2D AgentCollider { get { return agentCollider; } }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        agentCollider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2 destination)
     {
-        
+        transform.up = destination;
+        transform.position += (Vector3) destination * Time.deltaTime;
     }
 }
