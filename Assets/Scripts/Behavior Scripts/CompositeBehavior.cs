9802053f56a2c949cfd85f460faedb386f882e27
Thomas Ha <96492327+tha2026@users.noreply.github.com>
2023-12-28 12:57:20 -0500

    Created composite behavior script

---
+++ Assets/Scripts/Behavior Scripts/CompositeBehavior.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Composite")]
+public class CompositeBehavior : FlockBehavior
+{
+    public FlockBehavior[] behaviors;
+    public float[] weights;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        // Handle data mismatch situations where the number of behaviors does not match the number of weights
+        if (weights.Length != behaviors.Length)
+        {
+            Debug.LogError("Data mismatch in " + name, this);
+            return Vector2.zero;
+        }
+
+        Vector2 move = Vector2.zero;
+        for (int i = 0; i <  behaviors.Length; i++)
+        {
+            Vector2 partialMove = behaviors[i].CalculateMove(agent, context, flock);
+            partialMove *= weights[i];
+            if (partialMove != Vector2.zero)
+            {
+                if (partialMove.sqrMagnitude > weights[i] * weights[i])
+                {
+                    partialMove.Normalize();
+                    partialMove *= weights[i];
+                }
+
+                move += partialMove
+            }
+        }
+
+        return move;
+    }
+}
