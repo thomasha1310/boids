6cf1f380638de6d4d950ec157cb84d85d7e51a92
Thomas Ha <96492327+tha2026@users.noreply.github.com>
2023-12-28 13:07:03 -0500

    Created and set up steered cohesion behavior script

---
+++ Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+public class SteeredCohesionBehavior : FlockBehavior
+{
+
+    Vector2 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+
+        Vector2 cohesionMove = Vector2.zero;
+        foreach (Transform item in context)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+        cohesionMove /= context.Count;
+
+        cohesionMove -= (Vector2)agent.transform.position;
+        cohesionMove = Vector2.SmoothDamp(agent.transform.up, cohesionMove, ref currentVelocity, agentSmoothTime);
+
+        return cohesionMove;
+    }
+}
