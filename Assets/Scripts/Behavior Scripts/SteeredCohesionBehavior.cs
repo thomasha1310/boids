43134849734f0c7e703af145fdebac165b61f511
Thomas Ha <96492327+tha2026@users.noreply.github.com>
2023-12-28 13:10:05 -0500

    Created and set up steered cohesion behavior object

--- Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
+++ Assets/Scripts/Behavior Scripts/SteeredCohesionBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+[CreateAssetMenu(menuName = "Flock/Behavior/SteeredCohesion")]
 public class SteeredCohesionBehavior : FlockBehavior
 {
 
